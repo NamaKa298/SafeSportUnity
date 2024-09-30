852aa2ea808953328d0a3c6e8030b1020233a026
NamaKa298 <marion.saint-martin@protonmail.com>
2024-09-30 22:25:42 +0200

    premier commit

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Seo } from "@/ui/components/seo/seo";
+import { Seo } from "@/ui/components/seo";
 
 export default function Home() {
   return (
       <>
         <Seo title="SafeSportUnity" description="Description..." />
-        <h1>SafeSportUnity Just Do It Safely Un Projet cool !!!</h1>
+        <h1 className="text-3xl font-blod underline text-red-500">SafeSportUnity Just Do It Safely Un Projet cool !!!</h1>
           
       </>
   );
