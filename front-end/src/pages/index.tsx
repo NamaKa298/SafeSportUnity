dd46e10932fca9f8a8bf289ec493cf0aef4f81f5
BenBet21 <benoit.beti@gmail.com>
2024-10-02 09:36:09 +0200

    mise à jour

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -4,9 +4,28 @@ import { Typography } from "@/ui/design-system/typography/typography";
 export default function Home() {
   return (
       <>
-        <Seo title="SafeSportUnity" description="Description..." />
-        <Typography variant="h1">SafeSportUnity.com</Typography>
-          
+          <Seo title="SafeSportUnity" description="Description..." />
+            
+            <div className="space-y-5">
+              <Typography variant = "display" component = "div">
+                  SafeSportUnity.com
+              </Typography>
+              <Typography theme="primary" variant = "body-lg" component = "h1">
+                  SafeSportUnity.com
+              </Typography>
+              <Typography theme="secondary" variant="lead" component = "div">
+                  SafeSportUnity.com
+              </Typography>
+              <Typography variant = "body-sm" component = "div">
+                  SafeSportUnity.com
+              </Typography>
+              <Typography variant = "caption4" component = "div">
+                  SafeSportUnity.com
+              </Typography>
+              <Typography variant = "caption4" weight="medium" component = "div">
+                  SafeSportUnity.com
+              </Typography>
+            </div>
       </>
   );
 }
