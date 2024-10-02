ca0c8d99f73c56ff04c645e73019c104b6f643a8
NamaKa298 <marion.saint-martin@protonmail.com>
2024-10-02 09:39:34 +0200

    rajout des boutons

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,10 +1,39 @@
 import { Seo } from "@/ui/components/seo/seo";
+import { Button } from "@/ui/design-system/button/button";
 import { Typography } from "@/ui/design-system/typography";
 
 export default function Home() {
   return (
     <>
       <Seo title="SafeSportUnity" description="Description..." />
+      <div className="flex items-center gap-4 p-10">
+        <Button size="small">Accent</Button>
+        <Button size="small" variant="secondary">Secondary</Button>
+        <Button size="small" variant="outline">Accent</Button>
+        <Button size="small" variant="disabled" disabled>
+          Accent
+        </Button>
+      </div>
+
+      <div className="flex items-center gap-4 p-10">
+        <Button>Accent</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="outline">Accent</Button>
+        <Button variant="disabled" disabled>
+          Accent
+        </Button>
+      </div>
+
+      <div className="flex items-center gap-4 p-10">
+        <Button size="large">Accent</Button>
+        <Button size="large" variant="secondary">Secondary</Button>
+        <Button size="large" variant="outline">Accent</Button>
+        <Button size="large" variant="disabled" disabled>
+          Accent
+        </Button>
+      </div>
+
+      {/*
       <div className="space-y-5">
         <Typography>SafeSportUnity</Typography>
         <Typography theme="primary" variant="body-lg" component="h1">
@@ -23,6 +52,7 @@ export default function Home() {
           SafeSportUnity
         </Typography>
       </div>
+      */}
     </>
   );
 }
