0bb3df9ddde5bf42867c47dc17b4ee58a40435d2
BenBet21 <benoit.beti@gmail.com>
2024-10-04 10:59:19 +0200

    composant navigation part 1

--- front-end/src/config/firebase-config.js
+++ front-end/src/config/firebase-config.js
@@ -4,11 +4,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAE8rHxz6aACrNBgtcbGk39tRm_YagBdIk",
-    authDomain: "safesportunity-5bfeb.firebaseapp.com",
-    projectId: "safesportunity-5bfeb",
-    storageBucket: "safesportunity-5bfeb.appspot.com",
-    messagingSenderId: "135958327711",
-    appId: "1:135958327711:web:6d48c37f06e2a87a970daa"
+  authDomain: "safesportunity-5bfeb.firebaseapp.com",
+  projectId: "safesportunity-5bfeb",
+  storageBucket: "safesportunity-5bfeb.appspot.com",
+  messagingSenderId: "135958327711",
+  appId: "1:135958327711:web:6d48c37f06e2a87a970daa"
 };
 
 // Initialize Firebase
--- front-end/src/ui/design-system/button/button.tsx
+++ front-end/src/ui/design-system/button/button.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import { IconProps } from "@/types/iconProps";
 import clsx from "clsx";
 import React from "react";
