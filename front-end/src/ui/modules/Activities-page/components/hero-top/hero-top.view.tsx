3eabd247b389c790f29da3ae48de526ab4512b81
BenBet21 <benoit.beti@gmail.com>
2024-10-14 20:47:37 +0200

    final page activities

--- front-end/src/ui/modules/Activities-page/components/hero-top/hero-top.view.tsx
+++ front-end/src/ui/modules/Activities-page/components/hero-top/hero-top.view.tsx
@@ -3,13 +3,13 @@ import { Typography } from "@/ui/design-system/typography";
 import { Typewriter } from "../typewriter/typewriter";
 
 export const HeroTopView = () => {
-    const text = "With our web app, you can finally achieve your fitness goals with ease and motivation.We provide personalized training plans tailored to your level, goals, and schedule. Additionally, our geolocation feature allows you to discover and join group workout sessions organized near you.";
+    const text = "Achieve your fitness goals with ease and motivation! We provide personalized training plans tailored to your level, goals, and schedule ! Join group workout sessions organized near you!!";
 
     return (
         <div>
-          <Container className="w-[90%] max-w-full h-[100px] overflow-y-auto mx-auto">
+          <Container className="w-[90%] max-w-full h-[130px] overflow-y-auto mx-auto bg-primary-300 flex items-center justify-center">
             <div>
-              <Typography variant="caption1" component="h3" className="w-full break-words p-4"> {/* Padding ajouté */}
+              <Typography variant="h3" component="h3" className="w-full break-words p-4"> {/* Padding ajouté */}
                 <Typewriter text={text} speed={100} resetDelay={10000} />
               </Typography>
             </div>
